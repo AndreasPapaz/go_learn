f8251fe4cea643256973b14a279a8274354e7b60
Andreas Papaz <Andreas@sashas-macbook.lan>
2018-09-11 22:44:57 -0500

    return tuple set

--- cards/deck.go
+++ cards/deck.go
@@ -5,6 +5,25 @@ import "fmt"
 // use type to create "class" deck
 type deck []string
 
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Hearts", "Clubs", "Diamonds"}
+	cardValues := []string{"Ace", "Two", "Three", "Four", "Five"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, suit+" of "+value)
+		}
+	}
+
+	return cards
+}
+
+func deal(d deck, handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:]
+}
+
 func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
--- cards/main.go
+++ cards/main.go
@@ -3,10 +3,15 @@ package main
 func main() {
 	// var card string = "Ace of Spades"
 	// card := newCard()
-	cards := deck{newCard(), "Ace of Diamonds"}
-	cards = append(cards, "Six of Spades")
+	// cards := deck{newCard(), "Ace of Diamonds"}
+	// cards = append(cards, "Six of Spades")
+	cards := newDeck()
 
 	cards.print()
+	hand, remCards := deal(cards, 5)
+
+	hand.print()
+	remCards.print()
 }
 
 func newCard() string {
