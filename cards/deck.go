5271456ea2e4425d6d5ec8d082ef265a7dbe417d
AndreasPapaz <andreaspapaz@gmail.com>
2018-09-11 21:47:59 -0500

    switch comp

--- cards/deck.go
+++ cards/deck.go
@@ -1,1 +1,12 @@
 package main
+
+import "fmt"
+
+// use type to create "class" deck
+type deck []string
+
+func (d deck) print() {
+	for i, card := range d {
+		fmt.Println(i, card)
+	}
+}
--- cards/main.go
+++ cards/main.go
@@ -1,16 +1,12 @@
 package main
 
-import "fmt"
-
 func main() {
 	// var card string = "Ace of Spades"
 	// card := newCard()
-	cards := []string{newCard(), "Ace of Diamonds"}
+	cards := deck{newCard(), "Ace of Diamonds"}
 	cards = append(cards, "Six of Spades")
 
-	for i, card := range cards {
-		fmt.Println(i, card)
-	}
+	cards.print()
 }
 
 func newCard() string {
