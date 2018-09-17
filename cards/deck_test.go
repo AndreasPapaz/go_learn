f5e7e099c1a565db10729612517f177fa9fe3d5f
AndreasPapaz <andreaspapaz@gmail.com>
2018-09-17 18:58:46 -0500

    fin tests

--- cards/deck_test.go
+++ cards/deck_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestNewDeck(t *testing.T) {
 	d := newDeck()
@@ -8,4 +11,27 @@ func TestNewDeck(t *testing.T) {
 	if len(d) != 16 {
 		t.Error("expected 16 cards, but got ", len(d))
 	}
+
+	if d[0] != "Ace of Spades" {
+		t.Error("expected Ace of Spades, but got ", d[0])
+	}
+
+	if d[len(d)-1] != "Four of Diamonds" {
+		t.Error("expected last card of Fource of Diamonds, but got : ", d[len(d)-1])
+	}
+}
+
+func TestSaveToDeckAndNewDeckFromFile(t *testing.T) {
+	os.Remove("_decktesting")
+
+	deck := newDeck()
+	deck.saveToFile("_decktesting")
+
+	loadedDeck := readFromFile("_decktesting")
+
+	if len(loadedDeck) != 16 {
+		t.Error("expected 16 cards, but got ", len(loadedDeck))
+	}
+
+	os.Remove("_decktesting")
 }
